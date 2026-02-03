18896747954aef5eedf45d090084139e424a1709
PS45942-TruongThaiAn <antruonthai000@gmail.com>
2026-02-03 11:07:52 +0700

    Initial: Calculator project

---
+++ src/main/java/Calculator/Calculator.java
@@ -0,0 +1,17 @@
+package Calculator;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public double divide(int a, int b) {
+		if (b == 0)
+			throw new ArithmeticException("Không thể chia cho 0");
+		return (double) a / b;
+	}
+}
---
+++ src/test/java/CalculatorTest/CalculatorTest.java
@@ -0,0 +1,20 @@
+package CalculatorTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import Calculator.Calculator;
+
+public class CalculatorTest {
+	Calculator calc = new Calculator();
+
+	@Test
+	public void testAdd() {
+		Assert.assertEquals(calc.add(10, 5), 15);
+	}
+
+	@Test
+	public void testDivide() {
+		Assert.assertEquals(calc.divide(10, 2), 5.0);
+	}
+}
